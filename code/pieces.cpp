e1bdda7bcb40f2c34f55ab5c7c6429fb2e5663c3
Adam Slimi <adam.slimi@ensta-paris.fr>
2020-12-08 11:05:28 +0100

    création formes

---
+++ code/pieces.cpp
@@ -0,0 +1,4 @@
+#include "pieces.hpp"
+
+Shape::Shape() : _center[0](-1), _center[1](-1)
+{}
---
+++ code/pieces.hpp
@@ -0,0 +1,83 @@
+#ifndef __PIECES__
+#define __PIECES__
+
+#include <vector>
+
+
+class Shape {
+    protected:
+        std::vector<int> _center[2];
+        std::vector<int> _color[3];
+        std::vector<int> _distribution[6];
+
+    public:
+        Shape();
+        void move_right();
+        void move_left();
+        void move_down();
+        virtual void rotate_left() = 0;
+        virtual void rotate_right() = 0;
+
+};
+
+class I : public Shape {
+    public :
+        I();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+class J : public Shape {
+    public :
+        J();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+class O : public Shape {
+    public :
+        O();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+class L : public Shape {
+    public :
+        L();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+class T : public Shape {
+    public :
+        T();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+class Z : public Shape {
+    public :
+        Z();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+class S : public Shape {
+    public :
+        S();
+        virtual void rotate_left();
+        virtual void rotate_right();
+    
+}
+
+
+
+
+
+#endif
