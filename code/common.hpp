3c5e38fa003c9f0a23f6c137a9067328ddb3559c
Camille <camille.julliard@ensta-paris.fr>
2021-01-05 09:27:26 +0100

    modif main

--- code/common.hpp
+++ code/common.hpp
@@ -1,13 +1,16 @@
-#ifndef COMMON_HPP
-#define 
+#ifndef __COMMON_HPP__
+#define __COMMON_HPP__
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include "point.hpp"
 
-struct  Config
-{
-    static int SPEED = 1;
-};
+// struct  Config
+// {
+//     static int SPEED  = 1;
+
+// };
 
 
 #endif 
--- code/main.cpp
+++ code/main.cpp
@@ -2,8 +2,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-
+#include "point.hpp"
+#include "pieces.hpp"
+#include "grid.hpp"
 
 int main(int argc, char** argv){
-    return 0;
+        Grid grid_test;
+        //std::cout << " " << grid_test._grid[0][1].is_occupied() << std::endl;
+        std::vector<int> col=std::vector<int>(3,0);
+
+        return 0;
 }
